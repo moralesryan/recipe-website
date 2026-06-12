b44b5c2ebf7aa64ade0da48876a3b3fb3a8e0b5f
moralesryan <rmoralesaguilar@byupathway.edu>
2026-06-11 23:52:02 -0600

    Initialize filters module with category buttons and search bar setup

---
+++ scripts/filters.mjs
@@ -0,0 +1,17 @@
+//filters.mjs file will get the catefories from the API
+//and render the filter buttons on the home page
+
+import { fetchMealsbyCategory } from "./api.mjs";
+import { renderGrid } from "./main.mjs";
+
+
+//module 'level' variables to track the state of the filters
+let currentCategory = "All";
+let currentQuery = "";
+let allMeals = []; //this stores all meals for the current category 
+
+export function initFilters(categories) {
+    buildCategoryButtons(categories);
+    setupSearchBar();
+
+}
